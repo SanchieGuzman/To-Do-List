e833f221e872665f6e97807f840fca0971415213
Sanchie Earl Guzman <2232886@slu.edu.ph>
2024-09-09 15:02:39 +0800

    Fixed the logic for addTask method

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,18 @@
-const textInput = document.getElementById("text-input");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (textInput === "") {
+  const textInput = document.getElementById("text-input");
+  const textValue = textInput.value.trim();
+
+  if (textValue.length === 0) {
     alert("You haven't written a task");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = textInput.value;
+    li.innerHTML = textValue;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
   textInput.value = "";
 }
